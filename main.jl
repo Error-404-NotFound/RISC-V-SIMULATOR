d189503d50f047d187ee786dfa3aa2aabb0dc023
Jaimin Viramgama <jaim7viramgama@gmail.com>
2024-02-20 19:19:36 +0530

    main.jl updated

--- main.jl
+++ main.jl
@@ -2,10 +2,18 @@ include("sim.jl")
 include("parser.jl")
 include("utility.jl")
 
-file_path = "/home/aniket/Desktop/Project/RISC-V-SIMULATOR/test.asm"
+file_path = "./test.asm"
 program = []
+
+function convert_iostream_to_string(io::IO)
+    seekstart(io)  
+    str = read(io, String)
+    return str
+end
+
 try
     file = open(file_path, "r")
+    
     for line in eachline(file)
         if !contains(line, "any")
             modified_line = replace(line, r"\b\d+\b" => x -> string(parse(Int, x)))
@@ -15,6 +23,12 @@ try
             push!(program, modified_line)            
         end
     end
+
+    str  = convert_iostream_to_string(file)
+    if !check_assembly_structure(str) && !check_assembly_content(str)
+        println("Error: Assembly code has incorrect structure or content.")
+    end
+  
     close(file)
     println(program)
 catch err
@@ -25,11 +39,12 @@ function main()
     sim = processor_Init()
     sim.cores[1].program = program
     run(sim)
-    # println(sim.memory[1,2])
-    show_memory(sim)
     for i in 1:length(sim.cores)
         println(sim.cores[i].registers)
     end
+    print(sim.memory[2,2])
+    # println(sim.memory[1,2])
+    # show_memory(sim)
 end
 
 main()
