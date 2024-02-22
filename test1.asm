21846c60710f4d71b52bc428b5194809c2aaeeb4
Johri Aniket Manish <johrianiket04@gmail.com>
2024-02-22 12:53:38 +0530

    minor bug fixed

--- test1.asm
+++ test1.asm
@@ -1,18 +1,25 @@
 .data
-    array: .word 10,20,30
-    str: .string "Hello, World"
+    
 .text
-    li x1 5    
-    li x2 100  
-    li x3 0
+    li x1 0     #i
+    li x2 5     #n
+    li x3 0     #j
+    li x6 0     #sum
     j loop 
     li x5 200
 
 loop: 
     beq x1 x2 endloop
+    loop1:
+        beq x3 x2 exit
+        addi x6 x6 1
+        addi x3 x3 1
+        j loop1
+    
+exit:
     addi x1 x1 1
     j loop
-    li x5 600
 
 endloop:
     li x5 100
+
